ed0e2ccca8be500d49c4360810f88bd271b4c929
zsr2531 <warhaamer1337@gmail.com>
2020-07-25 10:36:51 +0200

    Give nice helper functions to BinaryOperatorKind

--- src/memeLang/ast/BinaryOperationKind.java
+++ src/memeLang/ast/BinaryOperationKind.java
@@ -4,5 +4,24 @@ public enum BinaryOperationKind {
     Addition,
     Subtraction,
     Multiplication,
-    Division
+    Division;
+
+    public int getBinaryOperatorPrecedence() {
+        return switch (this) {
+            case Addition -> 1;
+            case Subtraction -> 2;
+            case Multiplication -> 3;
+            case Division -> 4;
+        };
+    }
+
+    public static BinaryOperationKind fromChar(char character) {
+        return switch (character) {
+            case '+' -> Addition;
+            case '-' -> Subtraction;
+            case '*' -> Multiplication;
+            case '/' -> Division;
+            default -> null;
+        };
+    }
 }
